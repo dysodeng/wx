5a246bcfe4236930ee437e2f3c31f327821a744c
栖枝 <dysodengs@gmail.com>
2022-05-07 13:27:04 +0800

    [feature] 服务端消息处理

---
+++ base/message/cdata.go
@@ -0,0 +1,16 @@
+package message
+
+// CDATAText 文本域
+type CDATAText struct {
+	Text string `xml:",innerxml"`
+}
+
+// value2CDATA 值转换为CDATA
+func value2CDATA(value string) CDATAText {
+	return CDATAText{"<![CDATA[" + value + "]]>"}
+}
+
+// ptrValue2CDATA 值转换为指针型CDATA
+func ptrValue2CDATA(value string) *CDATAText {
+	return &CDATAText{"<![CDATA[" + value + "]]>"}
+}

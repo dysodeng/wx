855583ab380000e5f9500f053b9c2da67667b3c1
丹枫染秋色 <dysodengs@gmail.com>
2026-03-21 15:50:39 +0800

    feat: 对接企业微信-素材管理

--- work/media/media.go
+++ work/media/media.go
@@ -43,3 +43,113 @@ func (m *Media) Upload(mediaType string, filename string, fileData []byte) (*Upl
 
 	return &result.UploadResult, nil
 }
+
+// Get 获取临时素材
+// 返回文件内容和content-type
+func (m *Media) Get(mediaId string) ([]byte, string, error) {
+	accessToken, err := m.account.AccessToken()
+	if err != nil {
+		return nil, "", kernelError.New(0, err)
+	}
+
+	apiUrl := fmt.Sprintf("cgi-bin/media/get?access_token=%s&media_id=%s", accessToken.AccessToken, mediaId)
+	res, contentType, err := http.GetWithRespContentType(apiUrl)
+	if err != nil {
+		return nil, "", kernelError.New(0, err)
+	}
+
+	// 如果返回json，说明是错误响应
+	if contentType == "application/json" || contentType == "text/plain" {
+		var result kernelError.ApiError
+		err = json.Unmarshal(res, &result)
+		if err != nil {
+			return nil, "", kernelError.New(0, err)
+		}
+		if result.ErrCode != 0 {
+			return nil, "", kernelError.NewWithApiError(result)
+		}
+	}
+
+	return res, contentType, nil
+}
+
+// GetJssdk 获取高清语音素材
+// 返回文件内容和content-type
+func (m *Media) GetJssdk(mediaId string) ([]byte, string, error) {
+	accessToken, err := m.account.AccessToken()
+	if err != nil {
+		return nil, "", kernelError.New(0, err)
+	}
+
+	apiUrl := fmt.Sprintf("cgi-bin/media/get/jssdk?access_token=%s&media_id=%s", accessToken.AccessToken, mediaId)
+	res, contentType, err := http.GetWithRespContentType(apiUrl)
+	if err != nil {
+		return nil, "", kernelError.New(0, err)
+	}
+
+	// 如果返回json，说明是错误响应
+	if contentType == "application/json" || contentType == "text/plain" {
+		var result kernelError.ApiError
+		err = json.Unmarshal(res, &result)
+		if err != nil {
+			return nil, "", kernelError.New(0, err)
+		}
+		if result.ErrCode != 0 {
+			return nil, "", kernelError.NewWithApiError(result)
+		}
+	}
+
+	return res, contentType, nil
+}
+
+// UploadImage 上传图片
+// 上传图片得到图片URL，该URL永久有效
+func (m *Media) UploadImage(filename string, fileData []byte) (*UploadImageResult, error) {
+	accessToken, err := m.account.AccessToken()
+	if err != nil {
+		return nil, kernelError.New(0, err)
+	}
+
+	apiUrl := fmt.Sprintf("cgi-bin/media/uploadimg?access_token=%s", accessToken.AccessToken)
+	res, err := http.Upload(apiUrl, filename, fileData)
+	if err != nil {
+		return nil, kernelError.New(0, err)
+	}
+
+	var result uploadImageResponse
+	err = json.Unmarshal(res, &result)
+	if err != nil {
+		return nil, kernelError.New(0, err)
+	}
+	if result.ErrCode != 0 {
+		return nil, kernelError.NewWithApiError(result.ApiError)
+	}
+
+	return &result.UploadImageResult, nil
+}
+
+// AsyncUpload 异步上传临时素材
+// scene: 上传场景，目前仅支持1-客户联系
+func (m *Media) AsyncUpload(req AsyncUploadRequest) (*AsyncUploadResult, error) {
+	accessToken, err := m.account.AccessToken()
+	if err != nil {
+		return nil, kernelError.New(0, err)
+	}
+
+	apiUrl := fmt.Sprintf("cgi-bin/media/upload_by_url?access_token=%s", accessToken.AccessToken)
+	res, err := http.PostJSON(apiUrl, req)
+	if err != nil {
+		return nil, kernelError.New(0, err)
+	}
+
+	var result asyncUploadResponse
+	err = json.Unmarshal(res, &result)
+	if err != nil {
+		return nil, kernelError.New(0, err)
+	}
+	if result.ErrCode != 0 {
+		return nil, kernelError.NewWithApiError(result.ApiError)
+	}
+
+	return &result.AsyncUploadResult, nil
+}
--- work/media/types.go
+++ work/media/types.go
@@ -14,3 +14,34 @@ type uploadResponse struct {
 	kernelError.ApiError
 	UploadResult
 }
+
+// UploadImageResult 上传图片结果
+type UploadImageResult struct {
+	Url string `json:"url"`
+}
+
+// uploadImageResponse 上传图片响应
+type uploadImageResponse struct {
+	kernelError.ApiError
+	UploadImageResult
+}
+
+// AsyncUploadRequest 异步上传临时素材请求
+type AsyncUploadRequest struct {
+	Scene     int    `json:"scene"`
+	MediaType string `json:"media_type"`
+	UploadUrl string `json:"upload_url"`
+	FileName  string `json:"filename,omitempty"`
+	Md5       string `json:"md5,omitempty"`
+}
+
+// AsyncUploadResult 异步上传临时素材结果
+type AsyncUploadResult struct {
+	JobId string `json:"jobid"`
+}
+
+// asyncUploadResponse 异步上传临时素材响应
+type asyncUploadResponse struct {
+	kernelError.ApiError
+	AsyncUploadResult
+}
